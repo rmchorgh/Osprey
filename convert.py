faf3d66d755d45a0ad5422ff46bcfb28ea48f171
Richard McHorgh <mchorghr2@gmail.com>
2022-11-21 01:35:19 -0500

    added shf + fn combo

--- convert.py
+++ convert.py
@@ -1,60 +1,126 @@
-from adafruit_hid.keycode import Keycode as K
+from json import loads
+from kmk.keys import KC as kc, Key
+from kmk.modules.layers import Layers as L
 
-# selected cols e f rows 6 8
-class Keycode:
-    def lookup(x: str, side: int = 0):
-        if x is None:
-            return
+class Layers(L):
+    def __init__(self, km):
+        self.km = km
+        self.shfPressed = False
 
-        c = None
+    def before_hid_send(self, kbd):
+        if self.shfPressed and len(kbd.active_layers) == 1:
+            self.shfPressed = False
+            kbd.remove_key(kc.LSFT)
+            print("un fn shift")
+        elif not self.shfPressed and len(kbd.active_layers) > 2:
+            self.shfPressed = True
+            kbd.remove_key(Key(1003))
+            kbd.add_key(kc.LSFT)
+            print("fn shift")
 
-        l = len(x)
-        if l == 1:
-            if x.upper() in dir(K):
-                c = x.upper()
+class Keymap:
+    def asKC(self, key):
+        if key is None:
+            return None
+        elif key == "Lyp":
+            self.active = (min(self.maxLayers, self.active[0] + 1), 0)
+            print("trying layer", self.active)
+            try:
+                return kc.TO(self.order[self.active])
+            except KeyError:
+                return kc.TRNS
+        elif key == "Lyn":
+            self.active = (max(0, self.active[0] - 1), 0)
+            print("trying layer", self.active)
+            try:
+                return kc.TO(self.order[self.active])
+            except KeyError:
+                return kc.TRNS
+        elif key == "Fun":
+            if (self.active, 2) in self.order.keys():
+                print("on fun sublayer")
+                return kc.MO(self.order[self.active, 2])
+            return kc.TRNS
+        elif len(key) == 1:
+            cn = ord(key)
+
+            # is a number
+            if cn >= 48 and cn <= 57:
+                tc = f"N{key}"
             else:
+                tc = key
+        elif len(key) == 3:
+            try:
+                # is a function greater than f9
+                int(key[1:])
+                tc = key
+            except:
                 table = {
-                    "'": "QUOTE",
-                    "1": "ONE",
-                    "2": "TWO",
-                    "3": "THREE",
-                    "4": "FOUR",
-                    "5": "FIVE",
-                    "6": "SIX",
-                    "7": "SEVEN",
-                    "8": "EIGHT",
-                    "9": "NINE",
-                    "0": "ZERO",
+                    "Esc": "esc",
+                    "Tab": "tab",
+                    "Shf": "lsft",
+                    "Spc": "spc",
+                    "Sup": "left_super",
+                    "Alt": "lalt",
+                    "Bsp": "bksp",
+                    "Ctr": "lctl",
+                    "Ent": "ent",
+                    "Dho": "home",
+                    "Den": "end",
+                    "Dup": "up",
+                    "Ddo": "down",
+                    "Dle": "left",
+                    "Dri": "right",
+                    "Del": "del",
+                    "Pst": "trns",
                 }
-
-                if x in table.keys():
-                    c = table[x]
-        elif l == 2:
-            if x in dir(K):
-                c = x
-        elif l == 3:
-            table = {
-                "Shf": ["LEFT_SHIFT", "RIGHT_SHIFT"][side],
-                "Sup": ["LEFT_GUI", "RIGHT_GUI"][side],
-                "Tab": "TAB",
-                "Bsp": "BACKSPACE",
-                "Ent": "RETURN",
-                "Ctr": ["LEFT_CONTROL", "RIGHT_CONTROL"][side],
-                "Spc": "SPACEBAR",
-            }
-
-            if x in table.keys():
-                c = table[x]
-
-        if c is not None:
-            return getattr(K, c)
+                tc = table[key]
         else:
-            print(x)
-            print(sorted(dir(K)[5:]))
-            print("")
+            tc = "perc"
 
-    def parse(x: str):
-        if x is None:
-            return
+        return kc[tc.upper()]
 
-        return [Keycode.lookup(c) for c in x.split("+")]
+    def __init__(self, kbd):
+        self.layout = []
+        self.order = {}
+        self.active = (0, 0)
+        self.maxLayers = 0
+        layerIndex = -1
+
+        self.kbd = kbd
+        self.kbd.modules.append(Layers())
+        with open("layout.json", "r") as f:
+            j = loads(f.read())
+            for ikl, kl in enumerate(["mac"]):  #  j["layers"]
+                l = j["layers"][kl]
+                cols = [x for x in "fedcba"]
+                lm = [
+                    [None for x in range(10 * len(cols))]
+                    for y in range(1 + len(l["sublayers"]))
+                ]
+
+                self.maxLayers += 1
+                layerIndex += 1
+                self.order[(ikl, 0)] = layerIndex
+
+                for isl, sl in enumerate(l["sublayers"]):
+                    layerIndex += 1
+                    self.order[(ikl, isl + 1)] = layerIndex
+
+                for ci, c in enumerate(cols):
+                    for ri, char in enumerate(l[c]):
+                        index = (ri * 6) + ci
+
+                        if isinstance(char, list):
+                            for isl, sl in enumerate(char):
+                                if l["sublayers"][isl - 1] == "Shf" and sl == 0:
+                                    lm[isl][index] = kc.LSFT(self.asKC(char[0]))
+                                else:
+                                    lm[isl][index] = self.asKC(sl)
+                        else:
+                            lm[0][index] = self.asKC(char)
+                self.layout += lm
+
+        self.kbd.keymap = self.layout
+        print(self.layout[0])
+        # self.kbd.keymap = [[kc.A for x in range(60)]]
