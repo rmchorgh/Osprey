3308ec0ec2d845d9bf1bf6d88eeef7907299e9de
Richard McHorgh <mchorghr2@gmail.com>
2022-12-17 14:06:48 -0500

    maybe fixed windows

--- onboard/boot.py
+++ onboard/boot.py
@@ -1,9 +1,10 @@
 import supervisor
-
-# import usb_cdc
-# import storage
+import usb_hid
+import usb_cdc
+import storage
 
 supervisor.set_next_stack_limit(4096 + 4096)
 
-# storage.disable_usb_drive()
-# usb_cdc.disable()
+storage.disable_usb_drive()
+usb_cdc.disable()
+usb_hid.enable(boot_device=1)
