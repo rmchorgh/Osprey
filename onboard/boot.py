aea0e9ceedb89ddd9fa54212e26afc3126d810b9
Richard McHorgh <mchorghr2@gmail.com>
2023-01-17 20:50:07 -0500

    should toggle drive like on a,n press

--- onboard/boot.py
+++ onboard/boot.py
@@ -3,8 +3,22 @@ import usb_hid
 import usb_cdc
 import storage
 
+from board import GP3, GP19
+from digitalio import DigitalInOut, Direction, Pull
+
 supervisor.set_next_stack_limit(4096 + 4096)
 
-# storage.disable_usb_drive()
-# usb_cdc.disable()
-# usb_hid.enable(boot_device=1)
+# if GP19 x GP3 is high, dont disable cdc
+c = DigitalInOut(GP3)
+r = DigitalInOut(GP19)
+
+c.direction = Direction.OUTPUT
+r.switch_to_input(pull=Pull.DOWN)
+
+if not row.value:
+    storage.disable_usb_drive()
+    usb_cdc.disable()
+    usb_hid.enable(boot_device=1)
+
+r.deinit()
+c.deinit()
